20bad37578ecd3612f5fdb0fe5707c2fbef4de54
user.email <7630ash@gmail.com>
2024-03-27 17:06:50 +0530

    added some more problems

--- sorting_and_searching/Missing_Coin_Sum.cpp
+++ sorting_and_searching/Missing_Coin_Sum.cpp
@@ -21,43 +21,61 @@ signed main()
         coins.push_back(x);
     }
 
-    vector<vector<int>> dp(n+1, vector<int>(sum+1,0));
+
+    // for smaller inputs 
+    // vector<vector<int>> dp(n+1, vector<int>(sum+1,0));
     
-    for(int i =0;i<=n;i++)
-    {
-        for(int j = 0;j<=sum ; j++)
-        {
-            if(j==0)
-                dp[i][j] = 1;
-        }
-    }
+    // for(int i =0;i<=n;i++)
+    // {
+    //     for(int j = 0;j<=sum ; j++)
+    //     {
+    //         if(j==0)
+    //             dp[i][j] = 1;
+    //     }
+    // }
 
-    for(int i = 1;i<=n;i++)
-    {
-        for(int j = 1;j<=sum;j++)
-        {
-            if(coins[i-1] <= j)
-            {
-                dp[i][j] = dp[i-1][j-coins[i-1]] || dp[i-1][j];
-            }
-            else{
-                dp[i][j] = dp[i-1][j];
-            }
-        }
-    }
+    // for(int i = 1;i<=n;i++)
+    // {
+    //     for(int j = 1;j<=sum;j++)
+    //     {
+    //         if(coins[i-1] <= j)
+    //         {
+    //             dp[i][j] = dp[i-1][j-coins[i-1]] || dp[i-1][j];
+    //         }
+    //         else{
+    //             dp[i][j] = dp[i-1][j];
+    //         }
+    //     }
+    // }
 
-    int res = 0 ;
-    for(int i = 1;i<=sum;i++)
+    // int res = 0 ;
+    // for(int i = 1;i<=sum;i++)
+    // {
+    //     if(dp[n][i] == 0 )
+    //     {
+    //         res = i;
+    //         break;
+    //     }
+    // }
+    // if(res == 0)res = sum+1;
+    // cout<<res<<endl;
+    
+
+
+    // mthd - 2 
+    sort(coins.begin(),coins.end());
+    int target = 1;
+    for(int i =0;i<n;i++)
     {
-        if(dp[n][i] == 0 )
+        if(coins[i] > target)
         {
-            res = i;
             break;
         }
+        
+        target += coins[i];
     }
-    if(res == 0)res = sum+1;
-    cout<<res<<endl;
-    
+
+    cout<<target<<endl;
 
     return 0;
 }
